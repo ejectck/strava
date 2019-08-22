ca03f704959f236c798db21b7d5a2880e3f59373
Robert van Gent <rvangent@google.com>
2019-08-22 16:15:37 -0700

    add activity_type to upload; change commute/trainer to int32

--- api_uploads.go
+++ api_uploads.go
@@ -11,12 +11,13 @@ package strava
 
 import (
 	"context"
+	"fmt"
 	"io/ioutil"
 	"net/http"
 	"net/url"
-	"strings"
-	"fmt"
 	"os"
+	"strings"
+
 	"github.com/antihax/optional"
 )
 
@@ -27,7 +28,7 @@ var (
 
 type UploadsApiService service
 
-/* 
+/*
 UploadsApiService Upload Activity
 Uploads a new data file to create an activity from. Requires activity:write scope.
  * @param ctx context.Context - for authentication, logging, cancellation, deadlines, tracing, etc. Passed from http.Request or context.Background().
@@ -35,30 +36,31 @@ Uploads a new data file to create an activity from. Requires activity:write scop
      * @param "File" (optional.Interface of *os.File) -  The uploaded file.
      * @param "Name" (optional.String) -  The desired name of the resulting activity.
      * @param "Description" (optional.String) -  The desired description of the resulting activity.
-     * @param "Trainer" (optional.String) -  Whether the resulting activity should be marked as having been performed on a trainer.
-     * @param "Commute" (optional.String) -  Whether the resulting activity should be tagged as a commute.
+     * @param "Trainer" (optional.Int32) -  Whether the resulting activity should be marked as having been performed on a trainer.
+     * @param "Commute" (optional.Int32) -  Whether the resulting activity should be tagged as a commute.
      * @param "DataType" (optional.String) -  The format of the uploaded file.
      * @param "ExternalId" (optional.String) -  The desired external identifier of the resulting activity.
 
 @return Upload
 */
 
-type CreateUploadOpts struct { 
-	File optional.Interface
-	Name optional.String
+type CreateUploadOpts struct {
+	File        optional.Interface
+	Name        optional.String
 	Description optional.String
-	Trainer optional.String
-	Commute optional.String
-	DataType optional.String
-	ExternalId optional.String
+	Type        optional.String
+	Trainer     optional.Int32
+	Commute     optional.Int32
+	DataType    optional.String
+	ExternalId  optional.String
 }
 
 func (a *UploadsApiService) CreateUpload(ctx context.Context, localVarOptionals *CreateUploadOpts) (Upload, *http.Response, error) {
 	var (
-		localVarHttpMethod = strings.ToUpper("Post")
-		localVarPostBody   interface{}
-		localVarFileName   string
-		localVarFileBytes  []byte
+		localVarHttpMethod  = strings.ToUpper("Post")
+		localVarPostBody    interface{}
+		localVarFileName    string
+		localVarFileBytes   []byte
 		localVarReturnValue Upload
 	)
 
@@ -91,7 +93,7 @@ func (a *UploadsApiService) CreateUpload(ctx context.Context, localVarOptionals 
 		localVarFileOk := false
 		localVarFile, localVarFileOk = localVarOptionals.File.Value().(*os.File)
 		if !localVarFileOk {
-				return localVarReturnValue, nil, reportError("file should be *os.File")
+			return localVarReturnValue, nil, reportError("file should be *os.File")
 		}
 	}
 	if localVarFile != nil {
@@ -101,22 +103,25 @@ func (a *UploadsApiService) CreateUpload(ctx context.Context, localVarOptionals 
 		localVarFile.Close()
 	}
 	if localVarOptionals != nil && localVarOptionals.Name.IsSet() {
-		localVarFormParams.Add("name", parameterToString(localVarOptionals.Name.Value(), ""))
+		localVarQueryParams.Add("name", parameterToString(localVarOptionals.Name.Value(), ""))
 	}
 	if localVarOptionals != nil && localVarOptionals.Description.IsSet() {
-		localVarFormParams.Add("description", parameterToString(localVarOptionals.Description.Value(), ""))
+		localVarQueryParams.Add("description", parameterToString(localVarOptionals.Description.Value(), ""))
+	}
+	if localVarOptionals != nil && localVarOptionals.Type.IsSet() {
+		localVarQueryParams.Add("activity_type", parameterToString(localVarOptionals.Type.Value(), ""))
 	}
 	if localVarOptionals != nil && localVarOptionals.Trainer.IsSet() {
-		localVarFormParams.Add("trainer", parameterToString(localVarOptionals.Trainer.Value(), ""))
+		localVarQueryParams.Add("trainer", parameterToString(localVarOptionals.Trainer.Value(), ""))
 	}
 	if localVarOptionals != nil && localVarOptionals.Commute.IsSet() {
-		localVarFormParams.Add("commute", parameterToString(localVarOptionals.Commute.Value(), ""))
+		localVarQueryParams.Add("commute", parameterToString(localVarOptionals.Commute.Value(), ""))
 	}
 	if localVarOptionals != nil && localVarOptionals.DataType.IsSet() {
-		localVarFormParams.Add("data_type", parameterToString(localVarOptionals.DataType.Value(), ""))
+		localVarQueryParams.Add("data_type", parameterToString(localVarOptionals.DataType.Value(), ""))
 	}
 	if localVarOptionals != nil && localVarOptionals.ExternalId.IsSet() {
-		localVarFormParams.Add("external_id", parameterToString(localVarOptionals.ExternalId.Value(), ""))
+		localVarQueryParams.Add("external_id", parameterToString(localVarOptionals.ExternalId.Value(), ""))
 	}
 	r, err := a.client.prepareRequest(ctx, localVarPath, localVarHttpMethod, localVarPostBody, localVarHeaderParams, localVarQueryParams, localVarFormParams, localVarFileName, localVarFileBytes)
 	if err != nil {
@@ -136,47 +141,47 @@ func (a *UploadsApiService) CreateUpload(ctx context.Context, localVarOptionals 
 
 	if localVarHttpResponse.StatusCode < 300 {
 		// If we succeed, return the data, otherwise pass on to decode error.
-		err = a.client.decode(&localVarReturnValue, localVarBody, localVarHttpResponse.Header.Get("Content-Type"));
-		if err == nil { 
+		err = a.client.decode(&localVarReturnValue, localVarBody, localVarHttpResponse.Header.Get("Content-Type"))
+		if err == nil {
 			return localVarReturnValue, localVarHttpResponse, err
 		}
 	}
 
 	if localVarHttpResponse.StatusCode >= 300 {
 		newErr := GenericSwaggerError{
-			body: localVarBody,
+			body:  localVarBody,
 			error: localVarHttpResponse.Status,
 		}
-		
+
 		if localVarHttpResponse.StatusCode == 201 {
 			var v Upload
-			err = a.client.decode(&v, localVarBody, localVarHttpResponse.Header.Get("Content-Type"));
-				if err != nil {
-					newErr.error = err.Error()
-					return localVarReturnValue, localVarHttpResponse, newErr
-				}
-				newErr.model = v
+			err = a.client.decode(&v, localVarBody, localVarHttpResponse.Header.Get("Content-Type"))
+			if err != nil {
+				newErr.error = err.Error()
 				return localVarReturnValue, localVarHttpResponse, newErr
+			}
+			newErr.model = v
+			return localVarReturnValue, localVarHttpResponse, newErr
 		}
-		
+
 		if localVarHttpResponse.StatusCode == 0 {
 			var v Fault
-			err = a.client.decode(&v, localVarBody, localVarHttpResponse.Header.Get("Content-Type"));
-				if err != nil {
-					newErr.error = err.Error()
-					return localVarReturnValue, localVarHttpResponse, newErr
-				}
-				newErr.model = v
+			err = a.client.decode(&v, localVarBody, localVarHttpResponse.Header.Get("Content-Type"))
+			if err != nil {
+				newErr.error = err.Error()
 				return localVarReturnValue, localVarHttpResponse, newErr
+			}
+			newErr.model = v
+			return localVarReturnValue, localVarHttpResponse, newErr
 		}
-		
+
 		return localVarReturnValue, localVarHttpResponse, newErr
 	}
 
 	return localVarReturnValue, localVarHttpResponse, nil
 }
 
-/* 
+/*
 UploadsApiService Get Upload
 Returns an upload for a given identifier. Requires activity:write scope.
  * @param ctx context.Context - for authentication, logging, cancellation, deadlines, tracing, etc. Passed from http.Request or context.Background().
@@ -186,10 +191,10 @@ Returns an upload for a given identifier. Requires activity:write scope.
 */
 func (a *UploadsApiService) GetUploadById(ctx context.Context, uploadId int64) (Upload, *http.Response, error) {
 	var (
-		localVarHttpMethod = strings.ToUpper("Get")
-		localVarPostBody   interface{}
-		localVarFileName   string
-		localVarFileBytes  []byte
+		localVarHttpMethod  = strings.ToUpper("Get")
+		localVarPostBody    interface{}
+		localVarFileName    string
+		localVarFileBytes   []byte
 		localVarReturnValue Upload
 	)
 
@@ -236,40 +241,40 @@ func (a *UploadsApiService) GetUploadById(ctx context.Context, uploadId int64) (
 
 	if localVarHttpResponse.StatusCode < 300 {
 		// If we succeed, return the data, otherwise pass on to decode error.
-		err = a.client.decode(&localVarReturnValue, localVarBody, localVarHttpResponse.Header.Get("Content-Type"));
-		if err == nil { 
+		err = a.client.decode(&localVarReturnValue, localVarBody, localVarHttpResponse.Header.Get("Content-Type"))
+		if err == nil {
 			return localVarReturnValue, localVarHttpResponse, err
 		}
 	}
 
 	if localVarHttpResponse.StatusCode >= 300 {
 		newErr := GenericSwaggerError{
-			body: localVarBody,
+			body:  localVarBody,
 			error: localVarHttpResponse.Status,
 		}
-		
+
 		if localVarHttpResponse.StatusCode == 200 {
 			var v Upload
-			err = a.client.decode(&v, localVarBody, localVarHttpResponse.Header.Get("Content-Type"));
-				if err != nil {
-					newErr.error = err.Error()
-					return localVarReturnValue, localVarHttpResponse, newErr
-				}
-				newErr.model = v
+			err = a.client.decode(&v, localVarBody, localVarHttpResponse.Header.Get("Content-Type"))
+			if err != nil {
+				newErr.error = err.Error()
 				return localVarReturnValue, localVarHttpResponse, newErr
+			}
+			newErr.model = v
+			return localVarReturnValue, localVarHttpResponse, newErr
 		}
-		
+
 		if localVarHttpResponse.StatusCode == 0 {
 			var v Fault
-			err = a.client.decode(&v, localVarBody, localVarHttpResponse.Header.Get("Content-Type"));
-				if err != nil {
-					newErr.error = err.Error()
-					return localVarReturnValue, localVarHttpResponse, newErr
-				}
-				newErr.model = v
+			err = a.client.decode(&v, localVarBody, localVarHttpResponse.Header.Get("Content-Type"))
+			if err != nil {
+				newErr.error = err.Error()
 				return localVarReturnValue, localVarHttpResponse, newErr
+			}
+			newErr.model = v
+			return localVarReturnValue, localVarHttpResponse, newErr
 		}
-		
+
 		return localVarReturnValue, localVarHttpResponse, newErr
 	}
 
